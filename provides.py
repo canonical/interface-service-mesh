e5228e49efe146ac56985d56ef85d3f411fa6817
Kenneth Koski <kenneth.koski@canonical.com>
2020-10-21 15:28:59 -0500

    Avoid using `_raw` functions
    Since we're speaking JSON anyways, there's no need to use them directly

--- provides.py
+++ provides.py
@@ -1,12 +1,10 @@
-import json
-
 from charms.reactive import Endpoint
 
 
 class ServiceMeshProvides(Endpoint):
     def routes(self):
         return [
-            json.loads(relation.joined_units.received_raw['route'])
+            relation.joined_units.received['route']
             for relation in self.relations
             if relation.joined_units.received_raw.get('route')
         ]
